5441299f16c8c03ba2b957519977a0603c05edde
VictorZambrano <ingenieriainversazambrano@gmail.com>
2021-06-19 22:36:25 -0500

    crud

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-function App() {
+const App =() =>{
   return (
-    <div>Hola Mundo.</div>
+    <div className="container"  > 
+    <h1>CRUD App with Hooks  </h1>
+    <div className ="flex-row">
+      <div className="flex-large">
+        <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+           <h2> View users </h2>
+           </div>
+          </div>
+        </div>
     
   );
 }
