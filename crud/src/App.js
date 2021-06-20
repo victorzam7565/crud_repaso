c56a2aac6d1fbaed1faddc3fc74d2d860a356729
VictorZambrano <ingenieriainversazambrano@gmail.com>
2021-06-19 23:03:26 -0500

    crud

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-const App =() =>{
+import UserTable from './components/UserTable';
+function App () {
   return (
     <div className="container"  > 
     <h1>CRUD App with Hooks  </h1>
@@ -10,6 +10,7 @@ const App =() =>{
         </div>
         <div className="flex-large">
            <h2> View users </h2>
+           <UserTable/>
            </div>
           </div>
         </div>
